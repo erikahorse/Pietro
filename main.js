6dbffb2ddf4b2bd2402a9a934acadb0e680031d0
erikahorse <162718974+erikahorse@users.noreply.github.com>
2024-03-12 11:24:14 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://br.web.img2.acsta.net/c_310_420/pictures/22/08/11/20/52/3703489.jpg";
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://www.teenidols4you.com/blink/Actors/andrea-arru/andrea-arru-1697404231.jpg";
+}
+
